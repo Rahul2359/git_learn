09e1d473f447e48e8be66e0ed77344ea4a317b89
Rahul Barik <rahulkumarbarik150@gmail.com>
2024-08-26 23:23:32 +0530

    palindrom of a number

--- Main.java
+++ Main.java
@@ -1,59 +1,26 @@
-//Amstrong number
-//If the sum of  cube of all the digit of a number is equal to that number then the number is called Amstrong number,like 153=1+125+27   370,371,407,1634
-
+//Palindrome number
+//If we reverse a digit and it agian became that number then it is called palindrom num
 import java.util.*;
-/**
- * Main
- */
-public class Main {
-
-    public static void main(String[] args) {
+class Main{
+    public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the number");
         int num=sc.nextInt();
-        Amstrong(num);
-
-
-
+        palindrom(num);
     }
-    public static int power(int base,int exponent){
-        int result=1;
-        while (exponent!=0) {
-            result=result*base;
-            exponent--;
-        }
-        return result;
-    }
-
-
-    public static int digit_num(int a){
+    public static void palindrom(int num){
+        int num1=num;
+        int result=0;
         int i=0;
-        while(a!=0){
-            a=a/10;
-            i++;
+        while(num1!=0){
+            i=num1%10;
+            result=result*10+i;
+            num1/=10;
         }
-        return i;
-    }
-
-
-
-
-    public static void Amstrong(int num1){
-        double result=0;
-        int num=num1;
-        int digit=digit_num(num);
-        while(num!=0){
-            int digits=num%10;
-            result=result+power(digits,digit);
-            num=num/10;
-        }
-        if(result==num1){
-            System.out.println("The number is a armstrong number");
+        if(result==num){
+            System.out.println("The number is palindrom");
         }else{
-            System.out.println("the number is not a armstrong number");
+            System.out.println("The result is not palindrom");
         }
-
     }
-
-
 }
