cd58fd906abbc4ec9275793915205bb3c61a6be2
Rahul Barik <rahulkumarbarik150@gmail.com>
2024-08-30 19:53:50 +0530

    access array element

--- Main.java
+++ Main.java
@@ -1,26 +1,33 @@
-//Palindrome number
-//If we reverse a digit and it agian became that number then it is called palindrom num
+//Loop paterns
 import java.util.*;
 class Main{
-    public static void main(String args[]){
+    public static void main(String[] args) { 
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the number");
-        int num=sc.nextInt();
-        palindrom(num);
-    }
-    public static void palindrom(int num){
-        int num1=num;
-        int result=0;
+        int[] arr=new int[10];
+        int[][] arr1=new int[5][5];
+        int [] arr2={1,2,3,4,5};
+        int [][] arr3={{1,2,3,4,5},{1,2,3,4},{3,5,3,6,2}};
+        arr[1]=9;
+        arr1[1][2]=9;
         int i=0;
-        while(num1!=0){
-            i=num1%10;
-            result=result*10+i;
-            num1/=10;
+        int j=0;
+        
+        while(i<arr2.length){
+            System.out.println(arr2[i]);
+            i++;
         }
-        if(result==num){
-            System.out.println("The number is palindrom");
-        }else{
-            System.out.println("The result is not palindrom");
+        
+        while (j<arr3.length) {
+            int s=0;
+            while(s<arr3[j].length){
+                System.out.print(arr3[j][s]);
+                s++;
+            }
+            j++;
+            System.out.println();
+
+            
         }
-    }
+
+    } 
 }
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,17 +1,11 @@
-/**
- * tempCodeRunnerFile
- */
-public class tempCodeRunnerFile {
-
-    public static void main(String[] args) {
-        int a=2;
-        int b=3;
-        int result=;
-        while(b!=0){
-            result=result*a;
-            b--;
-
+int i=0;
+        while(i<arr.length){
+            int j=0;
+            while(i<arr.length){
+                int j=0;
+                while(j<arr[i].length){
+                    j++;
+                }
+                i++;
+            }
         }
-        System.out.println(result);
-    }
-}
