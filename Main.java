90cb375b9b8c5f0f41d287b5b2430ebd31cc31f8
Rahul Barik <rahulkumarbarik150@gmail.com>
2024-09-03 23:39:00 +0530

    reverse an array

--- Main.java
+++ Main.java
@@ -1,43 +1,22 @@
 import java.util.*;
 class Main{
-  public static void main(String[] args) {
-    Scanner sc=new Scanner(System.in);
-    int [] arr=ArrayUtility.inputArray();
-    System.out.println("Enter the number you want to delete from the array");
-    int num=sc.nextInt();
-    int [] new_arr=delete_element(arr, num);
-    ArrayUtility.display_arr(new_arr);
-
-
-  }
-  public static int occ(int []arr,int num){
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int arr[]=ArrayUtility.inputArray();
+        System.out.println("The reverse array is ");
+        int[] newArray=reverse_array(arr);
+        ArrayUtility.display_arr(newArray);
+    }
+   public static int[] reverse_array(int arr[]){
+    int size=arr.length;
+    int [] reverse_array=new int[size];
     int i=0;
-    int size=0;
     while(i<arr.length){
-      if(arr[i]==num){
-        size+=1;
-      }
-      i++;
-    }
-    return size;
-  }
-  public static int[] delete_element(int arr[],int num){
-    int occ=occ(arr, num);
-    if (occ==0) {
-      return arr;
-    }
-    int newSize=arr.length-occ;
-    int [] newArray=new int[newSize];
-    int i=0;
-    int j=0;
-    while(i<newArray.length){
-      if(arr[i]!=num){
-        newArray[j]=arr[i];
-        j++;
-      }
-      i++;
+        reverse_array[i]=arr[size-i-1];
+        i++;
     }
 
-    return newArray;
-  }
+
+    return reverse_array;
+   }
 }
